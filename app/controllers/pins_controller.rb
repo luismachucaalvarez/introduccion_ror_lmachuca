9384ca1e17e161af29f0c1c7288c489f30d15c49
Luis Machuca Alvarez <luis.machuca.alvarez@outlook.com>
2019-07-27 09:45:11 -0400

    Working with database and search

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,35 @@
 class PinsController < ApplicationController
   def index
-    @Pins = Pin.all
+    # Setup search
+    if params[:q].present?
+      @Pins= Pin.where('title like?',"%={params[q]}%")
+    else
+      @Pins = Pin.all
+    end
+  end
+s
+  def new
+    @pin = Pin.new
+#    @pin = Pin.create(params[:name])
   end
 
-  def show
+  def create
+    @pin = Pin.new(pin_params)
+
+    #@pin.save
+    #redirect_to pins_path, :notice => "Pin subido con exito"
+    if @pin.save
+      #redirect_to @pin
+      redirect_to :controller => 'home', :action => 'index'
+    else
+      render 'new'
+    end
   end
+
+  private
+    def pin_params
+      params.require(:pin).permit(:name, :image, :description)
+    end
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'pins/edit'
   get 'pins/index'
+  get 'pins/new'
+  get 'pins/edit'
+  get 'pins/create'
   get 'pins/show'
+  resources :pins
   #devise_for :users
+  #devise_for :users, controllers: {
+  #  sessions: 'users/sessions'
+  #}
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    registrations: 'users/registrations'
   }
   #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

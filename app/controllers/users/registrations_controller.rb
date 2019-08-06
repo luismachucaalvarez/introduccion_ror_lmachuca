804b2298fbb00215c97d9c4129c25bf4f1da7ca4
Luis Machuca Alvarez <luis.machuca.alvarez@outlook.com>
2019-08-05 21:28:23 -0400

    Corrigiendo errores de carga

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,14 +43,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
   #  devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname, :avatar])
-  # end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname, :avatar])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
